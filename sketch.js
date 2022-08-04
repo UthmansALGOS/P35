68a0145700fcbb93ecd9827da5d28bcbeda70cc4
UthmansALGOS <96424036+UthmansALGOS@users.noreply.github.com>
2022-08-04 19:06:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ var height;
 function preload(){
    bg =loadImage("cityImage.png");
    balloonImage1=loadAnimation("HotAirBallon01.png");
-   balloonImage2=loadAnimation("HotAirBallon01.png","Images/HotAirBallon01.png",
-   "Images/HotAirBallon01.png","HotAirBallon02.png","Images/HotAirBallon02.png",
-   "Images/HotAirBallon02.png","HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+   balloonImage2=loadAnimation("HotAirBallon01.png","HotAirBallon01.png",
+   "Images/HotAirBallon01.png","HotAirBallon02.png","HotAirBallon02.png",
+   "Images/HotAirBallon02.png","HotAirBallon03.png","HotAirBallon03.png","HotAirBallon03.png");
   }
 
 //Function to set initial environment
